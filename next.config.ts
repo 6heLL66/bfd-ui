c773ef872eb73a5e0006f3de626277dc402e704b
Никита Семенчук <n.semenchuk@texture.finance>
2025-03-20 13:33:32 +0300

    asd

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  webpack: config => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding')
+    return config
+  },
   images: {
     remotePatterns: [
       {
