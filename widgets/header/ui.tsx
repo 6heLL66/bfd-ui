f83181d05058a7d09a6e6909405c52fec57c87e3
n.semenchuk <n.semenchuk@tinkoff.ru>
2025-03-31 16:44:14 +0200

    asd

--- widgets/header/ui.tsx
+++ widgets/header/ui.tsx
@@ -181,26 +181,8 @@ export const Header = () => {
               <MobileNavLink href="/sale" onClick={toggleMobileMenu} disabled>
                 <span className="flex items-center gap-2">
                   Sale
-                  <svg 
-                    xmlns="http://www.w3.org/2000/svg" 
-                    width="20" 
-                    height="20" 
-                    viewBox="0 0 24 24" 
-                    fill="none" 
-                    stroke="currentColor" 
-                    strokeWidth="2" 
-                    strokeLinecap="round" 
-                    strokeLinejoin="round"
-                    className="opacity-70"
-                  >
-                    <rect width="18" height="11" x="3" y="11" rx="2" ry="2"></rect>
-                    <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
-                  </svg>
                 </span>
               </MobileNavLink>
-              
-              <MobileNavLink href="/staking/bfd" onClick={toggleMobileMenu}>Staking</MobileNavLink>
-              <MobileNavLink href="/staking/bgt" onClick={toggleMobileMenu}>Vault</MobileNavLink>
               
               <MobileNavLink href="/treasury" onClick={toggleMobileMenu}>Treasury</MobileNavLink>
               <MobileNavLink href={DOCS_LINK} external onClick={toggleMobileMenu}>
